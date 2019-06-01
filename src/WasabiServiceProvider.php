fdb3e6b4504a1c02d358938dca7a30030d0e4264
Zachary Claret-Scott <zachary.claretscott@probablyrational.com>
2019-06-01 13:37:42 +0000

    Add service provider

---
+++ src/WasabiServiceProvider.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Providers;
+
+use Storage;
+use Aws\S3\S3Client;
+use League\Flysystem\AwsS3v3\AwsS3Adapter;
+use League\Flysystem\Filesystem;
+use Illuminate\Support\ServiceProvider;
+
+class WasabiServiceProvider extends ServiceProvider
+{
+	/**
+	 * Register services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Bootstrap services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		Storage::extend('wasabi', function ($app, $config) {
+			$conf = [
+				'endpoint' => "https://" . $config['bucket'] . ".s3." . $config['region'] . "wasabisys.com/",
+				'bucket_endpoint' => true,
+				'credentials' => [
+					'key' => $config['key'],
+					'secret' => $config['secret'],
+				],
+				'region' => $config['region'],
+				'version' => 'latest',
+			];
+
+			$client = new S3Client($conf);
+
+			$adapter = new AwsS3Adapter($client, $config['bucket'], $config['root']);
+
+			$filesystem = new Filesystem($adapter);
+
+			return $filesystem;
+		});
+	}
+}
